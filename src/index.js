d6664dda078d53f61211965eb8c6afae1dad3b1e
brookebachman00 <yourpetlover4411@yahoo.com>
2020-03-09 13:24:33 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
-let addToy = false
 
-document.addEventListener("DOMContentLoaded", ()=>{
-  const addBtn = document.querySelector('#new-toy-btn')
-  const toyForm = document.querySelector('.container')
-  addBtn.addEventListener('click', () => {
-    // hide & seek with the form
-    addToy = !addToy
-    if (addToy) {
-      toyForm.style.display = 'block'
-    } else {
-      toyForm.style.display = 'none'
-    }
+fetchAllToys();
+
+function fetchAllToys(){
+ const toyContainer = document.getElementById("toy-collection")
+
+
+
+  fetch("http://localhost:3000/toys")
+  .then(resp => resp.json())
+  .then(function(toys){
+    toys.forEach(toy => {
+
+      toyContainer.innerHTML += renderSingleToy(toy)
+    });
   })
+}
 
-})
+//I want to iterate over the toys, to get each toy
+//then attach render single toy to the toy container
+
+function renderSingleToy(toy){
+  return `
+  <div class="card">
+    <h2>${toy.name}</h2>
+    <img src=${toy.image} class="toy-avatar" />
+    <p>${toy.likes} </p>
+    <button class="like-btn">Like <3</button>
+  </div>
+  `
+}
+
+function addAToy(){
+  
+}
